7af10ab89a0a256fc5e9de13f7bed7d24a4bf278
Ivo Georgiev <ivo@linvo.org>
2017-03-05 19:41:58 +0200

    callback now returns third arg for stats

--- sync.js
+++ sync.js
@@ -26,12 +26,12 @@ module.exports = function sync (API, name, all, params, cb) {
     doPush(function (err) {
       if (err) return cb(err)
       pushed = true
-      if (pulled && pushed) cb(null, pulled)
+      if (pulled && pushed) cb(null, pulled, { pull: pulled.length, push: pushed.length })
     })
     doPull(function (err, recv) {
       if (err) return cb(err)
       pulled = recv || true
-      if (pulled && pushed) cb(null, pulled)
+      if (pulled && pushed) cb(null, pulled, { pull: pulled.length, push: pushed.length })
     })
   })
 
